df3fa9a535ab85149b59b88d73173709c4f8f380
ValLR <valentinalopezrecabal@gmail.com>
2017-07-13 01:48:58 -0400

    funcion que valida primeros campos pero no reconoce select

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	/*creo todas las variables*/
+	var name = $("#name").val();
+	var lastName = $("#lastname").val();
+	var email = $("#input-email").val();
+	var password = $("#input-password").val();
+
+	if(/^[a-zA-Z()+$]/.test(name)===false){
+		return alert("Ingresa tu nombre");
+	}
+	else if(/^[a-zA-Z()+$]/.test(lastName)===false){
+		return alert("Ingresa tu apellido");
+	}
+	else if((/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email))===false||email===""){
+		return alert("Ingresa un email válido")
+	}
+	else if((password.length >= 6)===false || password === ""){
+		return alert("Tu contraseña debe tener al menos 6 caracteres")
+	}
+	else if(password === "123456" || password==="098765" || password === "password"){
+		return alert("Contraseña inválida, elige una distinta")
+	}
+	else{
+		return alert("gracias por registrarte")
+	}
+
 }
